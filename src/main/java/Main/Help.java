5698e6566caa46a1514564ef626d44fd3249fb1d
kryst <kryst@K-PC>
2022-01-28 17:02:18 +0100

    Help.java update

--- src/main/java/Main/Help.java
+++ src/main/java/Main/Help.java
@@ -37,6 +37,7 @@ public class Help extends ListenerAdapter {
 		embed.addField("Play song", "**'**p *or*   **'**play <youtube link / searchkey>", false);
 		embed.addField("Play music list", "**'**pq *or*   **'**playqueue <youtube link>", false);
 		embed.addField("Skip a music track", "**'**next *or*   **'**skip", false);
+		embed.addField("Delete a track from the queue", "**'**delete *or*   **'**remove <track number from the queue>", false);
 		embed.addField("Show current queue", "**'**q *or*   **'**queue", false);
 		embed.addField("Current Song", "**'**np *or*   **'**now playing", false);
 		embed.addField("Pause music bot", "**'**pause *or*   **'**stop", false);
