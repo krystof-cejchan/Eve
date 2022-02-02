5423a0ee368cb43c799ba19b76702188e70c3626
kryst <kryst@K-PC>
2022-02-02 22:02:53 +0100

    Python Files to copy from

---
+++ src/MyFilesAndDocuments/UnusedFILES/TemporaryTestFiles/Py/hello.py
@@ -0,0 +1,7 @@
+import sys
+
+print('Number of Arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+print('This is Python Code')
+print('Executing Python')
+print('hehe')
