ccc4b481fe125f0c30cbebbf4a75754f6eccdb38
TheKrystof701 <krystofcejchan@seznam.cz>
2022-02-05 16:43:30 +0100

    new Class added for getting and setting ID of last text channel
    note: sometimes this may be overwritten when a user sends another command before the bot is able to proccess the previous one

---
+++ src/main/java/Main/CurrentTextChannel.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class CurrentTextChannel {
+	static String id;
+
+	public CurrentTextChannel(String id) {
+		CurrentTextChannel.id = id;
+	}
+
+	public void setIid(String id) {
+		CurrentTextChannel.id = id;
+	}
+
+	public static String getId() {
+		return id;
+	}
+
+}
