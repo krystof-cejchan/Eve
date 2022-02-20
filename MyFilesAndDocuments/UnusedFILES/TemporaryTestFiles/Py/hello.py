0234c4f90c724860352d1cc01307a7c6cf74bde4
TheKrystof701 <krystofcejchan@seznam.cz>
2022-02-21 00:17:46 +0100

    Py Scripts reorganized

--- MyFilesAndDocuments/UnusedFILES/TemporaryTestFiles/Py/hello.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-print('Number of Arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
-print('This is Python Code')
-print('Executing Python')
-print('hehe')
