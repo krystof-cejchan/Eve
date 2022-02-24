2a19c03899b5fdd8557bdd47981e0fd78c8a354f
TheKrystof701 <krystofcejchan@seznam.cz>
2022-02-24 23:37:04 +0100

    PHP updated

--- src/main/java/External_Files/imagemerging.php
+++ src/main/java/External_Files/imagemerging.php
@@ -1,82 +1,37 @@
-<!DOCTYPE html>
-<html>
-    <style>
-    #imgs { 
-  width:500px;
-  height: 500px;
-  background-repeat: no-repeat, repeat;
-  background-image: 
-  url(
-  <?php echo ($_GET["url1"]); ?>), 
-  url(<?php echo ($_GET["url2"]); ?>);
-  background-blend-mode: multiply;
-}
-    </style>
-<head>
-</head>
-<body>
-  <!--  <form method="GET">
-            <table>
-            	<tr>
-            		<td>url1</td>
-            		<td><input name="url1" type="text" /></td>
-            	</tr>
-            	<tr>
-            		<td>url2</td>
-            		<td><input name="url2" type="text" /></td>
-            	</tr>
-			
-        	</table>
-        
-            
-            <input type="submit" value="Odeslat" />
-        </form >  -->
-        <div id="imgs"></div>
-      
-</body>
-</html>
 <?php
+mergeImg('http://kys.hys.cz/DISCORD%20BOT(S)/adam_rage.png', 'http://kys.hys.cz/DISCORD%20BOT(S)/honza_slightlyconfused.png');
 
-function get1($pic1) {
-$img1 = base64_encode(file_get_contents($pic1));
-/* add size */
-echo  '<img src="data:image/jpg;base64,'.$img1.'">';
+function mergeImg($img1, $img2) {
+$dest = imagecreatefrompng($img1);
+$src = imagecreatefrompng($img2);
+ 
+
+$dst_x=0;
+
+
+$dst_y=0;
+
+
+$src_x=20;
+
+
+$src_y=20;
+
+
+$src_width=165;
+
+
+$src_height=100;
+
+
+$alpha =100;
+imagecopymerge($dest, $src, $dst_x, $dst_y, $src_x, $src_y, $src_width, $src_height, $alpha);
+ 
+// Output and free from memory
+header('Content-Type: image/png');
+imagepng($dest);
+ 
+imagedestroy($dest);
+imagedestroy($src);
 }
-function get2($pic2) {
-
-$img2 = base64_encode(file_get_contents($pic2));
-
-echo  '<img src="data:image/jpg;base64,'.$img2.'">';
-}
-
-
 ?>
-
-
-
-<!--<!DOCTYPE html>
-<html>
-<head>
-<style>
-#pic { 
-  width: 400px;
-  height: 400px;
-  background-repeat: no-repeat, repeat;
-  background-image: url("https://memetemplates.in/uploads/1641286889.png"), url("https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg");
-  background-blend-mode: multiply;
-}
-</style>
-</head>
-<body>
-
-<div id="pic"></div>
-
-</body>
-</html>
--->
-
-
-
-
-
-
