d23160891f1da67b5622edafadb7ceaa0c4b8bc1
TheKrystof701 <krystofcejchan@seznam.cz>
2022-02-26 19:19:36 +0100

    read webpage and get data
    new class added for retrieving data from webpage to get size of function
    from php

--- src/main/java/commands/_Memes.java
+++ src/main/java/commands/_Memes.java
@@ -3,6 +3,7 @@ package commands;
 import java.util.ArrayList;
 
 import _library_class.LibraryClass;
+import memes__php.GetFunctionsFrom_PHP;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -10,13 +11,15 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class _Memes implements ICommands {
 
 	@Override
-	public void doTask(MessageReceivedEvent event) throws Exception {
+	public void doTask(MessageReceivedEvent event) throws Exception, NumberFormatException {
 		String profpic_1 = event.getAuthor().getAvatarUrl();
 		// example →
 		// http://eveuwu.g6.cz/memes/IMemes.php?profpic1=https://i.kym-cdn.com/entries/icons/mobile/000/037/458/coverpal.jpg&rnd=1
 		String website = "http://eveuwu.g6.cz/memes/IMemes.php";
 		String _GET_profpic1 = "?&profpic1=";
-		String _GET_rnd = "&rnd=" + String.valueOf(LibraryClass.generateRandomInt(1, 2));
+		int max = Integer
+				.valueOf(GetFunctionsFrom_PHP.getCount("http://eveuwu.g6.cz/memes/IMemes.php?&getFunctionCount"));
+		String _GET_rnd = "&rnd=" + String.valueOf(LibraryClass.generateRandomInt(1, max));
 		String distinguisher = "&" + event.getMessage().getId();
 
 		try {
---
+++ src/main/java/memes__php/GetFunctionsFrom_PHP.java
@@ -0,0 +1,41 @@
+package memes__php;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class GetFunctionsFrom_PHP {
+
+	public static String getCount(String weburl) {
+		// http://eveuwu.g6.cz/memes/IMemes.php?&getFunctionCount
+
+		try {
+			URL url = new URL(weburl);
+
+			Scanner sc = new Scanner(url.openStream());
+
+			StringBuffer sb = new StringBuffer();
+			while (sc.hasNext()) {
+				sb.append(sc.next());
+
+			}
+
+			String result = sb.toString();
+			System.out.println(result);
+
+			sc.close();
+			return result.replaceAll("<[^>]*>", "");
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+}
