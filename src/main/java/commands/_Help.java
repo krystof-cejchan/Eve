87b5fa59b26d7ad9652375d42073c2cefef71351
TheKrystof701 <krystofcejchan@seznam.cz>
2022-03-04 16:33:59 +0100

    Help command output fix

--- src/main/java/commands/_Help.java
+++ src/main/java/commands/_Help.java
@@ -11,52 +11,81 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class _Help implements ICommands {
 
 	@Override
-	public void doTask(MessageReceivedEvent event) {
+	public void doTask(MessageReceivedEvent event) throws IndexOutOfBoundsException {
 
-		EmbedBuilder embedBuilder = new EmbedBuilder();
-		Prefix pref = new Prefix();
+		try {
+			EmbedBuilder embedBuilder = new EmbedBuilder();
+			Prefix pref = new Prefix();
 
-		embedBuilder.clear();
-		int maxEmbedSize = 25;
+			embedBuilder.clear();
+			int maxEmbedSize = 25;
 
-		int cmdSize = CommandManager.getAllCommands().size();
-		embedBuilder.clear();
-		if (cmdSize > maxEmbedSize) {
+			int counter = 0;
 
-			for (int i = 0; i < cmdSize; i++) {
-				// for (int j = 0; j < Math.abs(i * cmdSize - (Math.abs(i * cmdSize -
-				// maxEmbedSize))); j++) {
-				for (int w = (i) * 25; w < (i * maxEmbedSize)
-						+ Math.abs(i * cmdSize - (Math.abs(i * cmdSize - maxEmbedSize))); w++) {
+			int cmdSize = CommandManager.getAllCommands().size();
+			embedBuilder.clear();
+			if (cmdSize > maxEmbedSize) {
+
+				for (int i = 0; i < cmdSize; i++) {
+					// for (int j = 0; j < Math.abs(i * cmdSize - (Math.abs(i * cmdSize -
+					// maxEmbedSize))); j++) {
+					for (int w = (i) * 25; w < (i * maxEmbedSize)
+							+ Math.abs(i * cmdSize - (Math.abs(i * cmdSize - maxEmbedSize))); w++) {
+						String allTriggers = "";
+
+						if (w < cmdSize) {
+
+							for (String trigger : CommandManager.getCommandbyId(w).getTriggers()) {
+								allTriggers += pref.getValue() + trigger + "   ";
+							}
+
+							embedBuilder.addField("**" + CommandManager.getCommandbyId(w).getName() + "**  :  ",
+									CommandManager.getCommandbyId(w).whatDoIDo() + "\n" + allTriggers, true);
+							counter++;
+						} else {
+							embedBuilder.setColor(LibraryClass.getRandomColor());
+							// embedBuilder.setTitle("All " + CommandManager.getAllCommands().size() + "
+							// commands:");
+							embedBuilder.setTitle("Showing " + counter + " commands");
+							counter = 0;
+							event.getMessage().replyEmbeds(embedBuilder.build()).queue();
+							embedBuilder.clear();
+
+							counter++;
+							return;
+						}
+					}
+					// }
+					embedBuilder.setColor(LibraryClass.getRandomColor());
+					// embedBuilder.setTitle("All " + CommandManager.getAllCommands().size() + "
+					// commands:");
+					embedBuilder.setTitle("Showing " + counter + " commands");
+					counter = 0;
+					event.getMessage().replyEmbeds(embedBuilder.build()).queue();
+					embedBuilder.clear();
+				}
+			} else {
+
+				for (ICommands iCommands : CommandManager.getAllCommands()) {
 					String allTriggers = "";
-					for (String trigger : CommandManager.getCommandbyId(w).getTriggers()) {
+					for (String trigger : iCommands.getTriggers()) {
 						allTriggers += pref.getValue() + trigger + "   ";
 					}
-					embedBuilder.addField("**" + CommandManager.getCommandbyId(w).getName() + "**  :  ",
-							CommandManager.getCommandbyId(w).whatDoIDo() + "\n" + allTriggers, true);
+
+					embedBuilder.addField("**" + iCommands.getName() + "**  :  ",
+							iCommands.whatDoIDo() + "\n" + allTriggers, true);
+
 				}
-				// }
 				embedBuilder.setColor(LibraryClass.getRandomColor());
 				embedBuilder.setTitle("All " + CommandManager.getAllCommands().size() + " commands:");
 				event.getMessage().replyEmbeds(embedBuilder.build()).queue();
 				embedBuilder.clear();
 			}
-		} else {
+		} catch (
 
-			for (ICommands iCommands : CommandManager.getAllCommands()) {
-				String allTriggers = "";
-				for (String trigger : iCommands.getTriggers()) {
-					allTriggers += pref.getValue() + trigger + "   ";
-				}
-
-				embedBuilder.addField("**" + iCommands.getName() + "**  :  ",
-						iCommands.whatDoIDo() + "\n" + allTriggers, true);
-
-			}
-			embedBuilder.setColor(LibraryClass.getRandomColor());
-			embedBuilder.setTitle("All " + CommandManager.getAllCommands().size() + " commands:");
-			event.getMessage().replyEmbeds(embedBuilder.build()).queue();
-			embedBuilder.clear();
+		Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
