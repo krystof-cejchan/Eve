ad39484032b5778166de08705c995edff3d2be65
TheKrystof701 <krystofcejchan@seznam.cz>
2022-03-07 23:46:28 +0100

    py script edited

--- src/main/java/py_scripts/_looped_voice_listening.py
+++ src/main/java/py_scripts/_looped_voice_listening.py
@@ -2,7 +2,9 @@ import speech_recognition as sr
 
 UserVoiceRecognizer = sr.Recognizer()
 
-while (1):
+takes = 0
+
+while takes < 3:
     try:
 
         with sr.Microphone() as UserVoiceInputSource:
@@ -14,10 +16,14 @@ while (1):
             UserVoiceInput_converted_to_Text = UserVoiceRecognizer.recognize_google(UserVoiceInput)
             UserVoiceInput_converted_to_Text = UserVoiceInput_converted_to_Text.lower()
             print(UserVoiceInput_converted_to_Text)
+            exit(0)
 
     except KeyboardInterrupt:
         # ctrl + c  interruption
         exit(0)
 
     except sr.UnknownValueError:
+        takes = takes + 1
         print("impossible to understand")
+
+
