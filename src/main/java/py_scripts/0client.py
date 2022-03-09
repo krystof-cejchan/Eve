bf323b9e491d75be73c85e2ccd1daecaa9147d47
TheKrystof701 <krystofcejchan@seznam.cz>
2022-03-09 19:54:59 +0100

    py scripts edited

--- src/main/java/py_scripts/0client.py
+++ src/main/java/py_scripts/0client.py
@@ -26,19 +26,25 @@ def audio_stream():
     payload_size = struct.calcsize("Q")
     while True:
         try:
+            print("1")
             while len(data) < payload_size:
-                packet = client_socket.recv(4 * 1024)  # 4K
+                print("2")
+                packet = client_socket.recv(1 * 1024)  # 4K
                 if not packet: break
                 data += packet
             packed_msg_size = data[:payload_size]
             data = data[payload_size:]
             msg_size = struct.unpack("Q", packed_msg_size)[0]
             while len(data) < msg_size:
-                data += client_socket.recv(4 * 1024)
+                print("3")
+                data += client_socket.recv(1 * 1024)
             frame_data = data[:msg_size]
             data = data[msg_size:]
             frame = pickle.loads(frame_data)
             stream.write(frame)
+            print("4")
+
+
 
         except:
 
