e7b5cc4c837b2e1eb07152057e8c73fe7198059e
TheKrystof701 <krystofcejchan@seznam.cz>
2022-04-14 00:13:48 +0200

    file clean-up
    jython del

---
+++ src/test/java/Database_class_onStart.java
@@ -0,0 +1,29 @@
+import db.Database;
+import db.DbCommands;
+import main.onStart.IOnStart;
+import objects.ScriptPathPointer;
+
+import java.io.IOException;
+
+public class Database_class_onStart implements IOnStart {
+
+    @Override
+    public void doYourPart() {
+
+        try {
+            Database db = new Database(ScriptPathPointer.dbPath);
+            DbCommands db_cmds = new DbCommands(db.getPath());
+
+            db.createNewDB_withWholePath();
+            db.connectToDB();
+
+            db_cmds.createTable(null);
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
