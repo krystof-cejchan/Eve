7e95fdc4295c8ae4d6d5c68af95a48e368a26f8e
TheKrystof701 <krystofcejchan@seznam.cz>
2022-04-22 16:30:14 +0200

    Kotlin added

--- src/main/java/voice/PythonASCII_Decoding.kt
+++ src/main/java/voice/PythonASCII_Decoding.kt
@@ -1,39 +1,38 @@
-package voice;
+package voice
 
 /**
  * @author krystof-cejchan
  * Decoding ASCII encoded text
  */
-public class PythonASCII_Decoding {
+object PythonASCII_Decoding {
     /**
      * Transforms an ascii text to normal readable text with utf-8 encoding
      * For example <u>[99 104 97 110 103 101 32 116 104 101 32 108 97 110 103 117 97 103 101 32 116 111 32 67 122 101 99 104 32 108 97 110 103 117 97 103 101]</u>
-     * <strong>equals to </strong><u>change the language to Czech language</u>
+     * **equals to **<u>change the language to Czech language</u>
      *
      * @param asciiEncodedText text encoded in ascii
      * @return decoded text from ascii
      */
-    public static String decodeASCIItext(String asciiEncodedText) {
-        try {
-            if (asciiEncodedText == null || (asciiEncodedText.chars().anyMatch(Character::isLetter)))
-                return asciiEncodedText;
+    @JvmStatic
+    fun decodeASCIItext(asciiEncodedText: String?): String? {
+        return try {
+            if (asciiEncodedText == null || asciiEncodedText.chars().anyMatch { codePoint: Int -> Character.isLetter(codePoint) }) return asciiEncodedText
 
 
             //String rawString = LibraryClass.runPyScript("C:\\Users\\vecer\\IdeaProjects\\Eve\\src\\main\\java\\external_files\\py_scripts\\soundfiletotext.py", "C:\\Users\\vecer\\Music\\sample.wav cs-CZ");
-            String[] ascii = asciiEncodedText.split(" ");
-            int[] asciiNumbers = new int[ascii.length];
-            for (int i = 0; i < ascii.length; i++) {
-                asciiNumbers[i] = Integer.parseInt(ascii[i]);
+            val ascii = asciiEncodedText.split(" ".toRegex()).toTypedArray()
+            val asciiNumbers = IntArray(ascii.size)
+            for (i in ascii.indices) {
+                asciiNumbers[i] = ascii[i].toInt()
             }
-
-            StringBuilder str = new StringBuilder();
-            for (int i : asciiNumbers) {
-                str.append((char) i);
+            val str = StringBuilder()
+            for (i in asciiNumbers) {
+                str.append(i.toChar())
             }
-            return str.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            str.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
         }
     }
 }
