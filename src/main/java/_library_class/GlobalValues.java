76170e622de18e3bbba1132f8e5a6aa7a80dd0f9
TheKrystof701 <krystofcejchan@seznam.cz>
2022-04-24 14:45:22 +0200

    back to java 2

---
+++ src/main/java/_library_class/GlobalValues.java
@@ -0,0 +1,23 @@
+package _library_class;
+
+public class GlobalValues {
+    /**
+     * [audio_player.QueueCommand]
+     */
+   public static int shownTrackCount = 30;
+
+    /**
+     * [voice.voice_and_listening.SpeechToText]
+     */
+   public static final int MAX_VALUE = 100;
+
+    /**
+     * [voice.voice_and_listening.SpeechToText]
+     */
+   public static final int MAX_SEC_AUDIO_RECORDING = 8;
+
+    /**
+     * [main.Listener]
+     */
+   public static final boolean ALLOW_NOT_IN_TEST_MODE = true;
+}
