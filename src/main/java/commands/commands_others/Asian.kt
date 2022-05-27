94c870c920691dfced2f38e0d23bf2a3f628ddc8
TheKrystof701 <krystofcejchan@seznam.cz>
2022-05-27 15:10:58 +0200

    minor change

--- src/main/java/commands/commands_others/Asian.kt
+++ src/main/java/commands/commands_others/Asian.kt
@@ -1,29 +1,32 @@
-package commands.commands_others;
+package commands.commands_others
 
-import _library_class.LibraryClass;
+import _library_class.LibraryClass
 
 /**
  * A class generating gifs sendEmotionalDamage, sendJesus
  */
-public class Asian {
+object Asian {
     /**
      * @return a random link from links
      */
-    public static String sendEmotionalDamage() {
-        String[] links = {"https://tenor.com/bLzfU.gif", "https://tenor.com/bKsTq.gif", "https://tenor.com/bKtZ4.gif",
-                "https://tenor.com/bOgeZ.gif", "https://tenor.com/bOco4.gif", "https://tenor.com/bM4pO.gif"};
-        return links[LibraryClass.generateRandomInt(0, links.length)];
+    @JvmStatic
+    fun sendEmotionalDamage(): String {
+        val links = arrayOf(
+            "https://tenor.com/bLzfU.gif", "https://tenor.com/bKsTq.gif", "https://tenor.com/bKtZ4.gif",
+            "https://tenor.com/bOgeZ.gif", "https://tenor.com/bOco4.gif", "https://tenor.com/bM4pO.gif"
+        )
+        return links[LibraryClass.generateRandomInt(0, links.size)]
     }
 
     /**
      * @return a random link from links
      */
-    public static String sendJesus() {
-
-        String[] links = {"https://tenor.com/bDxn5.gif", "https://tenor.com/bGgnd.gif", "https://tenor.com/bNFnu.gif",
-                "https://tenor.com/bOqal.gif", "https://tenor.com/bO3s3.gif", "https://tenor.com/bOQ0f.gif"};
-
-        return links[LibraryClass.generateRandomInt(0, links.length)];
+    @JvmStatic
+    fun sendJesus(): String {
+        val links = arrayOf(
+            "https://tenor.com/bDxn5.gif", "https://tenor.com/bGgnd.gif", "https://tenor.com/bNFnu.gif",
+            "https://tenor.com/bOqal.gif", "https://tenor.com/bO3s3.gif", "https://tenor.com/bOQ0f.gif"
+        )
+        return links[LibraryClass.generateRandomInt(0, links.size)]
     }
-
 }
