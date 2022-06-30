5612422927084fee48a895a88084a0796a9a6c75
TheKrystof701 <krystofcejchan@seznam.cz>
2022-06-30 23:31:07 +0200

    Database removal

--- src/test/java/db/db_query/CreateTable_1.sql
+++
@@ -1,6 +0,0 @@
- CREATE TABLE "discord_guilds"
-("id" INTEGER UNIQUE,
- "d_id"	INTEGER NOT NULL UNIQUE,
- "d_name"	TEXT NOT NULL,
- "d_prefix"	TEXT,"first_noticed"	TEXT,
- PRIMARY KEY("id" AUTOINCREMENT));
